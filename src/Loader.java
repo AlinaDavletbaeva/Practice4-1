f3acc8ffca7148eb08ec43e6175870307a09db8b
Alina <niimura.tsucumi@gmail.com>
2020-09-09 20:23:55 +0300

    Реализация простейшего класса Книга

---
+++ src/Loader.java
@@ -0,0 +1,14 @@
+public class Loader {
+
+    public static void main(String[] arg)
+    {
+
+        Book a= new Book();
+        a.setName("Портрет Дориана Грея");
+        a.setAuthor("Оскар Уайльд");
+        a.setPages(320);
+
+        System.out.println(a);
+
+    }
+}
